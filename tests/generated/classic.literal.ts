c82bb054b584687d3a4783aeb16d9b8062eabdba
josc146 <josStorer@outlook.com>
2026-06-02 12:38:48 +0800

    small fix

--- tests/generated/classic.literal.ts
+++ tests/generated/classic.literal.ts
@@ -1,11 +1,11 @@
+import * as E from 'genshin-ts/definitions/enum'
 import { g } from 'genshin-ts/runtime/core'
 import { configId, faction, guid, prefabId } from 'genshin-ts/runtime/value'
-import * as E from 'genshin-ts/definitions/enum'
 
 // AUTO-GENERATED: classic (literal)
 // Run: npx tsx scripts/generate-node-gia-tests.ts
 
-g.server({ id: 1073741843, mode: 'classic' }).on('whenEntityIsCreated', (_evt, f) => {
+g.server({ id: 1073741849, mode: 'classic' }).on('whenEntityIsCreated', (_evt, f) => {
   f.teleportPlayer(f.getSelfEntity(), [2, 3, 4], [3, 4, 5])
   f.reviveActiveCharacter(f.getSelfEntity())
   f.setCharacterSElementalEnergy(f.getSelfEntity(), 2.25)
